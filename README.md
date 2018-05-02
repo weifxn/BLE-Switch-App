3ca8963cef53c7bf6a57d7e966d7b905759a02be
wf <weifan2647@gmail.com>
2018-05-02 16:19:07 +0800

    add readme.md

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-# BLE-Switch-App
-An Android Bluetooth LE application.
- 
-[Nordic nRF Toolbox](https://github.com/NordicSemiconductor/Android-nRF-Toolbox
-)
-[nRF UART](https://github.com/NordicPlayground/Android-nRF-UART)
+# BLE-android
+
+
+## 1.0 Allow permission for bluetooth
+
+1. Go AndroidManifest.xml
+
+```html
+  <uses-sdk
+        android:minSdkVersion="18"
+        android:targetSdkVersion="18" />
+    <uses-permission android:name="android.permission.BLUETOOTH" />
+    <uses-permission android:name="android.permission.BLUETOOTH_ADMIN" />
+```
+
+
+## 2.0 Check bluetooth using Bluetooth Adapter
+1. Create bluetooth adapter object. 
+
+```java
+	private BluetoothAdapter mBtAdapter = null;
+
+
+```
+
+2. Check if Bluetooth is available in `onCreate` method.
+
+```java
+	mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+	
+	if(mBtAdapter == null) {
+		Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_LONG);
+		finish();
+		return;
+	}
+```
 
 
  
---
+++ app/src/main/java/com/dk/wf/ble_wallswitch/DeviceListActivity.java
@@ -0,0 +1,87 @@
+package com.dk.wf.ble_wallswitch;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeviceListActivity extends Activity {
+
+    // Bluetooth section
+    private BluetoothAdapter mBluetoothAdapter;
+    List<BluetoothDevice> deviceList;
+    private DeviceAdapter deviceAdapter;
+    private ServiceConnection onService = null;
+
+    // Rssi Value
+    Map<String, Integer> devRssiValues;
+}
+
+class DeviceAdapter extends BaseAdapter {
+    Context context;
+    List<BluetoothDevice> devices;
+    LayoutInflater inflater;
+
+    // Constructor for device adapter class object
+    public DeviceAdapter(Context context, List<BluetoothDevice> devices) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.devices = devices;
+    }
+
+    // Getter
+    @Override
+    public int getCount() {
+        return devices.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return devices.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    // Bluetooth device details
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewGroup vg;
+
+        if(convertView != null) {
+            vg = (ViewGroup) convertView;
+        }
+        else {
+            // show on device_element, list out in the select device page
+            vg = (ViewGroup) inflater.inflate(R.layout.device_element, null);
+        }
+
+        // collect the bluetooth device details
+        BluetoothDevice device = devices.get(position);
+        final TextView tvadd = ((TextView) vg.findViewById(R.id.address));
+        final TextView tvname = ((TextView) vg.findViewById(R.id.name));
+        final TextView tvpaired = ((TextView) vg.findViewById(R.id.paired));
+        final TextView tvrssi = ((TextView) vg.findViewById(R.id.rssi));
+
+        // RSSI section
+        tvrssi.setVisibility(View.VISIBLE);
+        byte rssival = (byte) devRssiValues.get(device.getAddress()).intValue();
+        if (rssival != 0) {
+            tvrssi.setText("Rssi = " + String.valueOf(rssival));
+        }
+
+
+    }
+}
